62243dbdf15b41cda9560502e8cf83b2d624c58f
Erik Kohagura <erik.vks@outlook.com>
2026-09-22 17:14:32 -0300

    Adaptação de pluralidade

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 alert("Boas vindas ao jogo do número secreto");
 let chute;
 let numeroSecreto = 5;
+let tentativas = 0;
 console.log(numeroSecreto);
 
 while (chute != numeroSecreto){
+    tentativas++;
     chute = prompt("Escolha um númnero entre 1 e 10");
     if (chute == numeroSecreto){
-        alert("O número secreto é " + numeroSecreto);
+        break;
     } else { 
         if (chute > numeroSecreto){
-        alert("Número secreto é menor que " + chute);
+            alert("Número secreto é menor que " + chute);
         }
         else {
             alert("O número secreto é maior que " + chute);
         }
     }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
